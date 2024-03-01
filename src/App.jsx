f469b681c7eb562efaf4586d72e0a2455f556dac
Joseph <jotienoochieng@gmail.com>
2024-03-01 13:06:39 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className=" min-h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
