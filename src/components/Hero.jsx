13c565d68f3f9fd0c1a0c23258c17be947535bd0
Joseph <jotienoochieng@gmail.com>
2024-03-02 11:16:09 +0300

    designed navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { bwmap, wmap } from "../assets";
+import { styles } from "../styles";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
+        <img
+          src={bwmap}
+          alt="world map"
+          className="w-full h-full sm:block hidden object-cover"
+        />
+      </div>
+      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
+        <img
+          src={wmap}
+          alt="world map"
+          className="w-full h-full sm:hidden block object-cover"
+        />
+      </div>
+      <section
+        className="relative flex sm:flex-row flex-col w-full h-screen mx-auto 
+        sm:bg-hero bg-hero-mobile overflow-hidden"
+      >
+        <div
+          className={`absolute inset-0 sm:top-[250px] top-[150px] 
+          lg:top-[150px] xl:top-[250px] ${styles.paddingX} 
+          max-w-7xl mx-auto flex flex-row items-start
+          justify-between gap-3`}
+        >
+          <div className="flex flex-col justify-center items-center mt-5 ml-3">
+            <div className="w-5 h-5 rounded-full bg-[#0a0a0a] sm:hidden" />
+            <div className="w-1 sm:h-80 h-40 bw-gradient sm:hidden" />
+          </div>
+
+          <div>
+            <h1
+              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}
+            >
+              Hi, I'm{" "}
+              <span
+                className="sm:text-battleGray sm:text-[90px] 
+                text-eerieBlack text-[50px] font-mova
+                font-extrabold uppercase"
+              >
+                Joseph
+              </span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
+              Lorem ipsum dolor sit amet. <br className="sm:block hidden" />
+              consectetur adipisicing elit deleniti, voluptas.
+            </p>
+          </div>
+          <div
+            className="w-screen flex flex-col items-start 
+            justify-center sm:-ml-[3rem] xxs:mt-4"
+          ></div>
+
+          <div></div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
